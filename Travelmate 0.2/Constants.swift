3f1e1a381558c3f9b7eb6b159525ac47aa556009
Muhammad <Muhammad.xami@gmail.com>
2020-02-10 19:51:28 +0500

    constants

---
+++ Travelmate 0.2/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Travelmate 0.2
+//
+//  Created by Muhammad on 10/02/2020.
+//  Copyright © 2020 Saami. All rights reserved.
+//
+
+import Foundation
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let LoginViewController = "LoginVC"
+        
+    }
+}
