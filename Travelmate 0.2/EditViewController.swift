f8e2eee599510f343ee015d91105368c503929d4
Ahmed Ayoub <ahmedayoub95@gmail.com>
2020-02-12 14:49:12 +0500

    Edit button

---
+++ Travelmate 0.2/EditViewController.swift
@@ -0,0 +1,22 @@
+//
+//  EditViewController.swift
+//  Travelmate 0.2
+//
+//  Created by MacBook  on 2/12/20.
+//  Copyright © 2020 Saami. All rights reserved.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
